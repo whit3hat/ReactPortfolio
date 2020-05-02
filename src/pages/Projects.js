dd3440f73d2349c01eb664517df9f45e85c263c9
Spencer Wood <spencer.wood@me.com>
2020-05-02 11:17:07 -0500

    created base of the Projects.js file

---
+++ src/pages/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Row, Container } from '../components/Grid';
+
+const Projects = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size='md-6'>
+
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Projects;
