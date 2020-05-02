a0178802b6b985d072a2f72f6e4a44281366020c
Spencer Wood <spencer.wood@me.com>
2020-05-02 11:15:11 -0500

    created base of the Home.js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import { Col, Row, Container } from '../components/Grid';
 
+const Home = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size='md-6'>
+
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Home;
