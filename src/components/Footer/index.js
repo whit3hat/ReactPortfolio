ddd9fe485d9f742f871550ef15ad0f505b1a95ea
Spencer Wood <spencer.wood@me.com>
2020-05-02 12:23:23 -0500

    created the footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className='page-footer font-small black'>
+            <div className='footer-copyright text-center py-3'>Spencer Wood © 2020</div>
+        </footer>
+    );
+}
+
+export default Footer;
