e6908642f59f1cb5c60dae34667b3c459db75d43
Spencer Wood <spencer.wood@me.com>
2020-05-02 11:20:36 -0500

    created the Nav component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <a className='navbar-brand' href='/'>
+                Spencer Wood
+            </a>
+        </nav>
+    );
+}
+
+export default Nav;
