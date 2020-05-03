cf73285e9c4ae5092f8ba9c5476b178f13f3895c
Spencer Wood <spencer.wood@me.com>
2020-05-02 22:50:56 -0500

    created the card templates and layout for the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,86 @@
 import React from 'react';
 import { Col, Row, Container } from '../components/Grid';
+import Footer from '../components/Footer';
 
 const Projects = () => {
     return (
         <Container fluid>
             <Row>
-                <Col size='md-6'>
-
+                <Col size='lg-3'>
+                    <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
+             </Col>
+             <Col size='lg-3'>
+                    <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
+            </Col>
+            <Col size='lg-3'>
+                    <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
+            </Col>
+           
+             <Col size='lg-3'>
+                <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
                 </Col>
-            </Row>
-        </Container>
+                    <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
+                <Col size='lg-4'>
+                    <div class="card">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title"><a>Card title</a></h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                {/* <!-- Button --> */}
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+                    </div>
+                    
+            </Col>
+        </Row>
+    <Footer />
+</Container>
     )
 }
 
