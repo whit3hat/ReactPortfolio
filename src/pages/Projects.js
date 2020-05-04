bae65a01159fc76c04cf2ec085a2674a18a5f5db
Spencer Wood <spencer.wood@me.com>
2020-05-04 17:25:09 -0500

    added assests folder with images to the src folder

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,72 +6,69 @@ const Projects = () => {
     return (
         <Container fluid>
             <Row>
-                <Col size='lg-3'>
+                <Col size='md-3'>
                     <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src="../assets/burger.png" alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Burger Shop</a></h4>
+                                <p class="card-text"></p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
+                                
                             </div>
                     </div>
              </Col>
-             <Col size='lg-3'>
+             <Col size='md-3'>
                     <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src="" alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Password Generator</a></h4>
+                                <p class="card-text"></p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
                             </div>
                     </div>
             </Col>
-            <Col size='lg-3'>
+            <Col size='md-3'>
                     <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src=" " alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Daily Planner</a></h4>
+                                <p class="card-text"> </p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
                             </div>
                     </div>
             </Col>
            
-             <Col size='lg-3'>
+             <Col size='md-3'>
                 <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src=" " alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Luke and Morty</a></h4>
+                                <p class="card-text"> </p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
                             </div>
                     </div>
                 </Col>
+                <Col size='md-3'>
                     <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src=" " alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Budget Tracker</a></h4>
+                                <p class="card-text"> </p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
                             </div>
                     </div>
-                <Col size='lg-4'>
+                </Col>
+                <Col size='md-3'>
                     <div class="card">
-                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+                        <img class="card-img-top" src=" " alt="Card image cap" />
                             <div class="card-body">
-                                <h4 class="card-title"><a>Card title</a></h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                content.</p>
+                                <h4 class="card-title"><a>Employee Tracker</a></h4>
+                                <p class="card-text"> </p>
                                 {/* <!-- Button --> */}
                                 <a href="#" class="btn btn-primary">Button</a>
                             </div>
