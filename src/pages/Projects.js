9097412c4d3c1d0e3946ff91bb878d60cdb88736
Spencer Wood <spencer.wood@me.com>
2020-05-06 18:20:18 -0500

    added reset css file, added properties for the cards on the projects page and removed an image not used

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Reset.css';
+import './Projects.css';
 import { Col, Row, Container } from '../components/Grid';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Burger from '../assests/burger.png';
 import Planner from '../assests/dailyplanner.png';
+import Password from '../assests/password.jpeg';
+import Quiz from '../assests/quiz.jpeg';
 
 
 const Projects = () => {
@@ -14,7 +18,7 @@ const Projects = () => {
                 <Col size="lg-6 md-3">
                     <div className="card">
                         <img className="card-img-top" src={Burger} alt="Card image cap" />
-                            <div className="card-body">
+                            <div className="card-body"> 
                                 <h4 className="card-title"><a>Burger Shop</a></h4>
                                 <p className="card-text"></p>
                                 {/* <!-- Button --> */}
@@ -25,7 +29,7 @@ const Projects = () => {
              </Col>
              <Col size="lg-6 md-3">
                     <div className="card">
-                        <img className="card-img-top" src="" alt="Card image cap" />
+                        <img className="card-img-top" src={Password} alt="Card image cap" />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Password Generator</a></h4>
                                 <p className="card-text"></p>
@@ -49,7 +53,7 @@ const Projects = () => {
            <Row>
              <Col size="lg-6 md-3">
                 <div className="card">
-                        <img className="card-img-top" src=" " alt="Card image cap" />
+                        <img className="card-img-top" src={Quiz} alt="Card image cap" />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Luke and Morty</a></h4>
                                 <p className="card-text"> </p>
