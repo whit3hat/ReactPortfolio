1ed873b5504dcddedb9d1ee72577dce9637d0d68
Spencer Wood <spencer.wood@me.com>
2020-05-29 22:50:53 -0500

    added the correct font awesome link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -15,7 +15,7 @@ function Nav() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <a className="nav-link" href="/Home" component={Home}>Profile</a>
+          <a className="nav-link" href="/" component={Home}>Profile</a>
         </li>
         <li className="nav-item active">
           <a className="nav-link" href="/Projects" component={Projects}>Portfolio <span className="sr-only">(current)</span></a>
