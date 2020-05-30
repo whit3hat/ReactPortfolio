77772e4ff5032b757ceb4a11d5298c5e23739df7
Spencer Wood <spencer.wood@me.com>
2020-05-30 12:27:36 -0500

    added style of 70% to the biopic on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row, Container } from '../components/Grid';
+import BioPic from '../assests/bio_pic.jpg';
 import Nav from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -9,11 +10,12 @@ const Home = () => {
             <Nav />
             <Row>
                 <Col size='md-6'>
-
+                    <img src={BioPic} style={{height: '70%'}} />
                 </Col>
                 <Col size='md-6'>
                     <text>
-                        I'm a father of two.
+                    I'm a husband, father of 2 children who is currently working full-time and has an unheathly obsession with memes and sour candy.<br /><br /> I'm taking the coding bootcamp because coding has been something that has always interested me and I'm looking to start a new career upon completion.
+                    In my freetime I enjoy watching movies with my family or alone.  I also enjoy lifting weights in my newly completed garage gym. <br /><br />Sometimes when I get downtime before bed I enjoy surfing social media or Reddit for a good laugh.
                     </text>
                 </Col>
             </Row>
