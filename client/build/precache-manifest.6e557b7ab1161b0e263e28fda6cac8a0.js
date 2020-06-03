ed9828ed9124c6f2281e153d552beaff1fa9fc4c
Spencer Wood <spencer.wood@me.com>
2020-06-03 11:54:32 -0500

    created the client folder, ran the build service and moved the src,public and build into client

---
+++ client/build/precache-manifest.6e557b7ab1161b0e263e28fda6cac8a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dd12e79b04bb3fc6a58d37ed2fb5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a237bf5ee06a51fad229",
+    "url": "/static/css/main.98f8ceb0.chunk.css"
+  },
+  {
+    "revision": "0f6bf087080e86851836",
+    "url": "/static/js/2.e95c58dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e95c58dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a237bf5ee06a51fad229",
+    "url": "/static/js/main.b84db031.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "e939dab0eeac2ac7c093fbbc2f4ede96",
+    "url": "/static/media/BudgetTracker.e939dab0.png"
+  },
+  {
+    "revision": "6d0c8fc225b0af0e4a05d0bb7b99168b",
+    "url": "/static/media/DailyPlanner.6d0c8fc2.png"
+  },
+  {
+    "revision": "bd874761e0d1d083f05bdd21cc2aac8b",
+    "url": "/static/media/EmployeeTracker.bd874761.png"
+  },
+  {
+    "revision": "7030c0df41d857ee42a5520ea78e0964",
+    "url": "/static/media/bio_pic.7030c0df.jpg"
+  },
+  {
+    "revision": "b77f30e44676f39bb2993f6d5984aa45",
+    "url": "/static/media/burger.b77f30e4.png"
+  }
+]);
