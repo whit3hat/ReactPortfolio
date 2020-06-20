df73f87545139cbdff6c878c7166484309acdf45
Spencer Wood <spencer.wood@me.com>
2020-06-20 16:01:16 -0500

    created cards for the information on the contact page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Contact.css';
-import { Col, Row, Container } from 'reactstrap';
+import { Col, Row, Container, Card, CardBody, CardText } from 'reactstrap';
 import Nav from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -8,8 +8,36 @@ const Contact = () => {
 
     return (
         <div>
-            <Nav />
-            <Footer />
+            <Container className='themed-container' fluid={true} >
+               <Nav /> 
+                    <Row>
+                        <Col s='2'>
+                        <Card>
+                        <CardBody>
+                            <CardText>
+                                Have a question?
+                                Let's get to know each other. How can I be useful to your service?
+                                Please feel free to reach out to me. I would be delighted to answer all your questions or concerns.
+                            </CardText>
+                        </CardBody>
+                        </Card>
+                         </Col>
+
+                        <Col s='2'>
+                            <Card>
+                                <CardBody>
+                                    <CardText>
+                                        Feel free to call, email or connect on LinkedIn.
+                                        
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                
+            </Container>
+           <Footer />
         </div>
     )
 };
