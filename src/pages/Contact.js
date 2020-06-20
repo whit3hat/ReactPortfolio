a24675cef45077b00e6807e9274bf3a5ab640f2e
Spencer Wood <spencer.wood@me.com>
2020-06-20 13:14:31 -0500

    created default layout on the contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+import { Col, Row, Container } from 'reactstrap';
+import Nav from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Contact = () => {
+
+    return (
+        <div>
+            <Nav />
+            <Footer />
+        </div>
+    )
+};
+
+export default Contact;
