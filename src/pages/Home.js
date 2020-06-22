e6f1dad52d3733558de4240c01435e4bc98f5a5e
Spencer Wood <spencer.wood@me.com>
2020-06-21 20:48:46 -0500

    added text to the home page, added font style to the title on the home page and added padding to the homepage like other pages for the nav bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import './Home.css';
 import { Col, Row, Container } from 'reactstrap';
 import Nav from '../components/Navbar';
 import Footer from '../components/Footer';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <Nav />
+        <div id='home' style={{ paddingTop:'100px' }}>
             <Container className='themed-container' fluid={true}>
-
+            <Nav />
+                <div>
+                    <Row>
+                        <div id='greeting' style={{ fontFamily: 'Permanent Marker', fontSize: '50px' }}>
+                            <p>Hello, I'm Spencer!</p>
+                        </div>
+                    </Row>
+                    <Row>
+                        Full Stack Web Developer based in Kansas City who enjoys building Web
+                        Apps and brining them to life!
+                    </Row>
+                </div>
             </Container>
             <Footer />
         </div>
