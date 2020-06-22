f21cbc1739a33b58dd3d7aebdc9b02b848339bd0
Spencer Wood <spencer.wood@me.com>
2020-06-21 21:19:49 -0500

    added font colors to the text on the main page and moved the text to the right a bit on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,17 @@ import './Home.css';
 const Home = () => {
     return (
         <div id='home' style={{ paddingTop:'100px' }}>
-            <Container className='themed-container' fluid={true}>
+            <Container className='themed-container' fluid={true} style={{ paddingLeft: '50px' }} >
             <Nav />
                 <div>
                     <Row>
-                        <div id='greeting' style={{ fontFamily: 'Permanent Marker', fontSize: '50px' }}>
+                        <div id='greeting' style={{ fontFamily: 'Permanent Marker', fontSize: '50px', color: '#D0E1F9' }}>
                             <p>Hello, I'm Spencer!</p>
                         </div>
                     </Row>
                     <Row>
-                        <div id='homeBio' style={{ fontFamily: 'Ubuntu' }}>
-                            <p>Full Stack Web Developer based in Kansas City who enjoys building Web
+                        <div id='homeBio' style={{ fontFamily: 'Ubuntu' , color: '#283655'}}>
+                            <p>Full Stack Web Developer based in Kansas City <br/> who enjoys building Web
                                 Apps and brining them to life!</p> 
                         </div>
                     </Row>
