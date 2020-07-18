2bdea7efa76d39ba2a98c1d3d4ef89808562b6fd
Spencer Wood <spencer.wood@me.com>
2020-07-17 20:56:51 -0500

    fixed the navbar to work for smaller screens and also moved the icons to the right

--- src/components/Navbar2/index.js
+++ src/components/Navbar2/index.js
@@ -18,9 +18,15 @@ function Nav2 () {
       <ReactStrap.Nav.Link href="/Bio" component={Bio}>Profile</ReactStrap.Nav.Link>
       <ReactStrap.Nav.Link href="/Projects" component={Projects}>Projects</ReactStrap.Nav.Link>
       <ReactStrap.Nav.Link href="/Contact" component={Contact}>Contact</ReactStrap.Nav.Link>
-      <ul className="navbar-nav nav-flex-icons">
+    </ReactStrap.Nav>
+    {/* <ReactStrap.Form inline>
+      <ReactStrap.FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <ReactStrap.Button variant="outline-success">Search</ReactStrap.Button>
+    </ReactStrap.Form> */}
+
+<ul className="navbar-nav nav-flex-icons">
      <li className="nav-item">
-          <a href = "https://github.com/whit3hat" className="nav-link"><i className="fab fa-github fa-lg"></i></a>
+        <a href = "https://github.com/whit3hat" className="nav-link"><i className="fab fa-github fa-lg"></i></a>
          </li>
            <li className="nav-item">
           <a href="https://www.linkedin.com/in/spencer-wood-73158b61/" className="nav-link"><i className="fab fa-linkedin fa-lg"></i></a>
@@ -29,8 +35,6 @@ function Nav2 () {
              <a href={Resume} target='_blank' className="nav-link"><i className="far fa-file-pdf fa-lg"></i></a>
            </li>
        </ul>
-    </ReactStrap.Nav>
-
   </ReactStrap.Navbar.Collapse>
 </ReactStrap.Navbar>
     )
