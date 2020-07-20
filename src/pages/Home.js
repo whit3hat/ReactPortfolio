24c38d1650e07e3a1e9f2a86f01e96d00b4ebe05
Spencer Wood <spencer.wood@me.com>
2020-07-19 20:24:00 -0500

    removed the divs from the home.js page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,18 +12,15 @@ const Home = () => {
            
                 <Row>
                     <Col xs={12} s={12}>
-                        <div id='greeting' style={{ fontFamily: 'Permanent Marker', fontSize: '50px', color: '#D0E1F9' }}>
-                            <p>Hello, I'm Spencer!</p>
-                        </div>
+                            <p id='greeting' style={{ fontFamily: 'Permanent Marker', fontSize: '50px', color: '#D0E1F9' }}>Hello, I'm Spencer!</p>
+                        
                     </Col>
                 </Row>
                 
                     <Row id='bioRow'>
                         <Col xs={12} s={12} >
-                        <div id='homeBio' style={{ fontFamily: 'Ubuntu' , color: '#283655' }}>
-                            <p>Full Stack Web Developer based in Kansas City <br/> who enjoys building Web
+                            <p id='homeBio' style={{ fontFamily: 'Ubuntu' , color: '#283655' }}>Full Stack Web Developer based in Kansas City <br/> who enjoys building Web
                                 Apps and brining them to life!</p> 
-                        </div>
                         </Col>
                     </Row>   
                     <Row> 
