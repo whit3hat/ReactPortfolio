c5b1f7e642c3ebe8c3cc08089b01da22e020e469
Spencer Wood <spencer.wood@me.com>
2020-07-22 22:04:54 -0500

    fixed the issues with padding for the card images on the projects page. They are now centered and will go into a single line on smaller devices

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Projects.css';
-import { Col, Row, Container } from 'react-bootstrap';
+
+import { Col, Row, Container, Card, Button } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import Burger from '../assests/burger.png';
 import Planner from '../assests/DailyPlanner.png';
@@ -8,28 +8,29 @@ import Budget from '../assests/BudgetTracker.png'
 import Password from '../assests/password.jpeg';
 import EmployeeTracker from '../assests/EmployeeTracker.png';
 import Quiz from '../assests/quiz.jpeg';
-
+import './Projects.css';
 
 const Projects = () => {
     return (
-        <Container className='themed-container' fluid={true}>          
-            <Row>
+        <Container fluid>          
+            <Row className='justify-content-md-center'>
                 <Col sm='12' lg='4'>
-                    <div className="card">
-                        <img className="card-img-top" src={Burger} alt="" />
-                            <div className="card-body"> 
-                                <h4 className="card-title">Burger Shop</h4>
-                                <p className="card-text"></p>
-                                {/* <!-- Button --> */}
-                                <div className='d-flex justify-content-center'>
-                                <a href="https://github.com/whit3hat/BurgerShop " className="btn btn-primary">GitHub Link</a>
-                                </div>
-                            </div>
-                    </div>
+                    
+                  <Card >
+                    
+                        <Card.Img variant='top' id='cardImg' src={Burger} />
+                    
+                        
+                            <Card.Body>
+                                <Card.Title>Burger Shop</Card.Title>
+                                <Button variant='primary'>GitHub</Button>
+                            </Card.Body>
+                    </Card>
+                   
              </Col>
              <Col sm='12' lg='4'>
                     <div className="card">
-                        <img className="card-img-top" src={Password} alt="" />
+                        <img className="card-img-top" id='cardImg'src={Password} />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Password Generator</a></h4>
                                 <p className="card-text"></p>
@@ -42,7 +43,7 @@ const Projects = () => {
             </Col>
             <Col sm='12' lg='4'>
                     <div className="card">
-                        <img className="card-img-top" src={Planner} alt="" />
+                        <img className="card-img-top" id='cardImg' src={Planner} />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Daily Planner</a></h4>
                                 <p className="card-text"> </p>
@@ -58,7 +59,7 @@ const Projects = () => {
            <Row>
            <Col sm='12' lg='4'>
                 <div className="card">
-                        <img className="card-img-top" src={Quiz} alt="" />
+                        <img className="card-img-top" id='cardImg' src={Quiz} />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Luke and Morty</a></h4>
                                 <p className="card-text"> </p>
@@ -71,7 +72,7 @@ const Projects = () => {
                 </Col>
                 <Col sm='12' lg='4'>
                     <div className="card">
-                        <img className="card-img-top" src={Budget} alt="" />
+                        <img className="card-img-top" id='cardImg' src={Budget} />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Budget Tracker</a></h4>
                                 <p className="card-text"> </p>
@@ -84,7 +85,7 @@ const Projects = () => {
                 </Col>
                 <Col sm='12' lg='4'>
                     <div className="card">
-                        <img className="card-img-top" src={EmployeeTracker} alt="" style={{ alignSelf: 'center' }}/>
+                        <img className="card-img-top" id='cardImg' src={EmployeeTracker} />
                             <div className="card-body">
                                 <h4 className="card-title"><a>Employee Tracker</a></h4>
                                 <p className="card-text"> </p>
