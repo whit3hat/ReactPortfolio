49e5966e265b6ce15461d5e8f552e6042de03130
Spencer Wood <spencer.wood@me.com>
2020-08-13 12:43:29 -0500

    deleted build folder and mainest file from the pubilc folder to test heroku deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Nav from './components/Navbar';
+// import Nav from './components/Navbar';
 import Home from './components/Home';
 import Bio from './components/Bio';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-
+import Resume from './assests/Resume.pdf';
 import NoMatch from './components/NoMatch';
+import './style.css'
 
 function App() {
   return (
@@ -18,7 +19,19 @@ function App() {
        <li><Link to={'/projects'} className='nav-link'> Projects </Link></li>
        <li><Link to={'/contact'} className='nav-link'> Contact </Link></li>
      </ul>
+     <ul className="navbar-nav nav-flex-icons">
+     <li className="nav-item">
+        <a href = "https://github.com/whit3hat" className="nav-link"><i className="fab fa-github fa-lg"></i></a>
+         </li>
+           <li className="nav-item">
+          <a href="https://www.linkedin.com/in/spencer-wood-73158b61/" className="nav-link"><i className="fab fa-linkedin fa-lg"></i></a>
+           </li>
+           <li className="nav-item">
+             <a href={Resume} target='_blank' className="nav-link"><i className="far fa-file-pdf fa-lg"></i></a>
+           </li>
+       </ul>
      </nav>
+     {/* Disabled the navbar */}
      {/* <Nav /> */}
      <Switch>
          <Route exact path='/' component={Home} />
